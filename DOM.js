8a054427e9d06259a83fab20ca459d3f6ceba26f
Davi Almeida Matta da Silva <davialmeidamatta@gmail.com>
2026-07-13 19:09:29 -0300

    feat: Botão de cancelar também fecha o modal adição

--- DOM.js
+++ DOM.js
@@ -1,9 +1,12 @@
 
 const botao_abrir_adicionar = document.querySelector(".add_itens")
 const botao_fechar_adicionar = document.querySelector(".botao_fechar");
-
+const modal_adicionar =document.querySelector ("#modal_adicionar")
+const botao_cancelar_adicionar = document.querySelector(".botao_cancelar")
 
 botao_abrir_adicionar.addEventListener('click', () => {modal_adicionar.showModal(); 
 });
 botao_fechar_adicionar.addEventListener('click', () => {modal_adicionar.close();});
+botao_cancelar_adicionar.addEventListener('click', () => {modal_adicionar.close();});
+
 
